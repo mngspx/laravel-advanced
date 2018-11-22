4524dc2ca1ba11f88241b5eec8769cc79ac453d3
Pham Phuong <phamphuong.svit@gmail.com>
2018-11-22 17:17:31 +0700

    add branch logging

--- app/Http/Controllers/ErrorsLoggingController.php
+++ app/Http/Controllers/ErrorsLoggingController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Log;
 class ErrorsLoggingController extends Controller
 {
-    //
+    public function index(){
+        Log::info('Test ghi log info',['id'=>1]);
+        Log::emergency('Test ghi log emergency');
+        Log::alert('Test ghi log alert');
+        Log::critical('Test ghi log critical');
+        Log::error('Test ghi log error');
+        Log::warning('Test ghi log warning');
+        Log::notice('Test ghi log notice');
+        Log::debug('Test ghi log debug');
+    }
 }
